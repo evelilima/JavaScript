5b053dcefc93a51e97cbaa3264b9219feb0d849a
Éveli Lima <105569006+evelilima@users.noreply.github.com>
2022-06-01 14:47:42 -0300

    Módulo D

---
+++ modulo_d/motor.js
@@ -0,0 +1,41 @@
+var bomba = 'cesto quebrado'
+
+if (bomba == 'não funciona') {
+     console.log('Analizar problema e fazer orçamento.')
+} 
+else if (bomba == 'funciona'){
+    console.log('Enterga.')
+}
+else if (bomba == 'motor') {
+    console.log('Enrrolar motor')
+} 
+else if (bomba == 'rolamento') {
+    console.log('Trocar rolamento.')
+
+} 
+else if (bomba == 'eixo enferrujado' || 'eixo vazando') {
+
+    if (bomba == 'eixo enferrujado') {
+        console.log('Encamizar eixo.')
+    } 
+    else if (bomba == 'eixo vazando') {
+        console.log('Trocar selo mecânico.')
+    } 
+} else if (bomba == 'tampa rachada' || 'cesto quebrado') {
+
+    if (bomba == 'tampa rachada') {
+        console.log('Trocar tampa hidráulica.')
+    }
+    else if (bomba == 'cesto quebrado') {
+        console.log('Trocar cesto coletor.')
+    }
+}
+
+   
+        
+
+
+   
+    
+    
+    
