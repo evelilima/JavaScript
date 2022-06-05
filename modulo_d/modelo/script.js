077c40de14c88710b756c42c3d7e8207b0a6326d
Éveli Lima <105569006+evelilima@users.noreply.github.com>
2022-06-04 22:55:53 -0300

    Módulo D

--- modulo_d/modelo/script.js
+++ modulo_d/modelo/script.js
@@ -1,22 +1,24 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById ('imagem') 
+    var saud = window.document.getElementById('saud')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 8
     
     msg.innerHTML = `Agora são ${hora} horas.` 
     if (hora >= 0 && hora < 12){
-        //BOM DIA!
         img.src = 'modelo/imagens/manha_250.png'
         document.body.style.background = '#d6d7d1'
+        saud.innerHTML = 'Bom Dia!'
     }else if (hora >= 12 && hora <= 18){
-        //BOA TARDE!
         img.src = 'modelo/imagens/tarde_250.png'
         document.body.style.background = '#5462a3'
+        saud.innerHTML = 'Boa Tarde!'
     }else{
-        //BOA NOITE!
         img.src = 'modelo/imagens/noite_250.png'
         document.body.style.background = '#263650'
+        saud.innerHTML = 'Boa Noite!'
     }
 }
 
