58c5dfa70e019307dc09326ad8da027767937aaa
Éveli Lima <105569006+evelilima@users.noreply.github.com>
2022-06-15 23:41:00 -0300

    Módulo E

---
+++ modulo_e/aula14ex/ex016/script.js
@@ -0,0 +1,7 @@
+function contar() {
+    var inicio = document.getElementById('n1')
+    var fim = document.getElementById('n2')
+    var passo = document.getElementById('n3')
+    var res = document.getElementById('res')
+        
+}
